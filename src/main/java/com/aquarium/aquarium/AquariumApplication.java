9f703d4862ed7f6315639262fe96e114c9569d1e
maksym345 <maksym.wilk@studenci.collegiumwitelona.pl>
2025-11-17 18:06:53 +0100

    DODAŁEM IGNOROWANIE ŁĄCZENIA DO BAZY DANYCH AZURE (przenosimy się na aws) DODAŁEM PRZYKŁADOWY REST API

--- src/main/java/com/aquarium/aquarium/AquariumApplication.java
+++ src/main/java/com/aquarium/aquarium/AquariumApplication.java
@@ -2,12 +2,18 @@ package com.aquarium.aquarium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 public class AquariumApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AquariumApplication.class, args);
     }
-
 }
