7d84c59373e7c1e43ac2084a9b71fdf6db229f5f
robertkuzba <robert.kuzba@studenci.collegiumwitelona.pl>
2025-12-08 20:08:29 +0100

    Aktualizacja Security Config

--- src/main/java/com/aquarium/aquarium/SecurityConfig.java
+++ src/main/java/com/aquarium/aquarium/SecurityConfig.java
@@ -11,9 +11,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
     }
